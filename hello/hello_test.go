c33536fed9fc8da714ce3df7e30927a2d39b2d1a
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-05-05 23:17:10 +0900

    refs:テスト変更

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -9,16 +9,9 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
-
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
