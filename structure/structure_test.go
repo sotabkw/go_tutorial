6c04e0df7246172501571246870ea4331a27d832
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-05-09 03:31:21 +0900

    res:test修正

--- structure/structure_test.go
+++ structure/structure_test.go
@@ -62,3 +62,23 @@ func TestAreaThird(t *testing.T) {
 	}
 
 }
+
+func TestAreaForth(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.AreaSecond()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
