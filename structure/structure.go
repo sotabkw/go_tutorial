6982dc3382336c38819e4588f64a4a6c060d5b0f
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-05-08 20:32:03 +0900

    res:テストリファクタ

--- structure/structure.go
+++ structure/structure.go
@@ -1,9 +1,19 @@
 package structure
 
+import "math"
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+func (r Rectangle) AreaSecond() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) AreaSecond() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structure/structure_test.go
+++ structure/structure_test.go
@@ -21,3 +21,25 @@ func TestArea(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestAreaSecond(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.AreaSecond()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
+}
