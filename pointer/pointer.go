f4ead0588b5385f5d02de4b93a96e766198bd966
Sota <watanabeso@crunchtimer.jp>
2021-05-11 02:47:12 +0900

    refs:関数修正

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,9 +1,9 @@
 package pointer
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount BitCoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
