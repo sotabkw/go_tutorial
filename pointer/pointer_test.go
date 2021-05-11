733e539532a9f7ea3f3d8fde62eb0e78c94ee2bc
Sota <watanabeso@crunchtimer.jp>
2021-05-12 03:53:46 +0900

    add:pointerテスト追加

--- pointer/pointer_test.go
+++ pointer/pointer_test.go
@@ -18,3 +18,34 @@ func TestWallet(t *testing.T) {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
+
+func TestWalletSecond(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(BitCoin(10))
+
+		got := wallet.Balance()
+
+		want := BitCoin(10)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: BitCoin(20)}
+
+		wallet.Withdraw(BitCoin(10))
+
+		got := wallet.Balance()
+
+		want := BitCoin(10)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
